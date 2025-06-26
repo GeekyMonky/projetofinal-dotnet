890afd1649920471651e5136e3919aa6fc411ca4
Susana Ribeiro <susana.1994@live.com.pt>
2025-06-26 19:23:23 +0100

    Instalação dos 4 NuGet packages de Microsoft.EntityFrameworkCore. Configuração do BusinessContext.

---
+++ ProjetoFinal/ProjetoFinal.BusinessContext/BusinessContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal.BusinessContext
+{
+    public class BusinessContext: DbContext, IBusinessContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SQL6033.site4now.net;Initial Catalog=db_abaa2b_eletrotudo;User Id=db_abaa2b_eletrotudo_admin;Password=4#qzUHW3yKMSX@t");
+        }
+    }
+}
