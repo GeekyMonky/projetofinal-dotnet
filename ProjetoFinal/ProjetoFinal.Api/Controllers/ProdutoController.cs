e980ff86d4ce236e987c7cd9a1af2de958c436a7
Pedro Matias <pedromatias555@gmail.com>
2025-07-03 19:23:19 +0100

    ProdutoController  0.1 terminado.

--- ProjetoFinal/ProjetoFinal.Api/Controllers/ProdutoController.cs
+++ ProjetoFinal/ProjetoFinal.Api/Controllers/ProdutoController.cs
@@ -1,17 +1,41 @@
 using Microsoft.AspNetCore.Mvc;
+using ProjetoFinal.Shared;
 
 namespace ProjetoFinal.Api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("api/[controller]")]
 public class ProdutoController : ControllerBase
 {
-    private readonly ILogger<ProdutoController> logger;
+    private readonly IBusinessContext businessContext;
 
     public ProdutoController(ILogger<ProdutoController> logger)
     {
-        this.logger = logger;
+        _businessContext = businessContext;
     }
 
     
+    //Endpoint
+    [HttpGet("/produtos")]
+    public async Task<IActionResult> GetProdutos()
+    {
+        logger.LogInformation("Obtendo lista de produtos");
+        //Class do Produto do Shared.
+        List<Produto> produtoList  = new List<Produto>
+     
+        foreach (var produto in produtos)
+        {
+            var produtoDto = new Produto
+            {
+                Id = produto.Id,
+                Nome = produto.Nome,
+                Preco = produto.Preco,
+                Descricao = produto.Descricao
+            };
+
+        }
+
+        return Ok(produtoList);
+    }
+
 }
--- ProjetoFinal/ProjetoFinal.Api/Program.cs
+++ ProjetoFinal/ProjetoFinal.Api/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+
+builder.Services.AddDbContext<IBusinessContext, BusinessContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
