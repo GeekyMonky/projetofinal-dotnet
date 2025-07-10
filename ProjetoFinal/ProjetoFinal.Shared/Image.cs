3a889ad823e1fb4b476bd88960828583684b4042
Susana Ribeiro <susana.1994@live.com.pt>
2025-07-10 19:42:33 +0100

    Add entity classes and update database context
    This commit introduces several new entity classes (`Product`, `Category`, `Image`, `StockMovement`) and a base entity class (`BaseEntity`) in the `ProjetoFinal.BusinessContext.Entities` namespace. The `BusinessContext` class is updated with `DbSet` properties for these entities, and the connection string has been modified to point to a new database.
    
    Migration files have been created to establish the new database schema, including tables for the new entities. The `IBusinessContext` interface has been updated to manage these entities effectively. Additionally, the `BaseEntity` class has been relocated to the `ProjetoFinal.Shared` namespace, and the `Produto` class has been removed to standardize naming conventions. Redundant folder entries have also been cleaned up in the project file.

---
+++ ProjetoFinal/ProjetoFinal.Shared/Image.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoFinal.Shared
+{
+    public class Image: BaseEntity
+    {
+        public new int Id { get; set; }
+        public string Url { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
